295714e999c1b1e915ee4b97020b6c3efad29dda
Rayna Levinson <rlevinso@redhat.com>
2020-06-03 12:09:56 -0700

    gnmt tekton pipeline

---
+++ gnmt/drop_pipeline.sh
@@ -0,0 +1,7 @@
+oc delete pipeline/gnmt
+oc delete pipelinerun/gnmt-pipeline-run
+oc delete pipelineresource/gnmt-build-image
+oc delete pipelineresource/gnmt-repo
+oc delete task/gnmt-buildah
+oc delete task/gnmt-dataset
+oc delete task/gnmt-run
